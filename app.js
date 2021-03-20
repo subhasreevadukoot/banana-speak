072a65b3ebcab6156c61a0518fae7b9c58db75ba
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-03-20 17:28:06 +0000

    Banana Speak App completed

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
-var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL= "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslation(text){
-    return serverURL+"?"+"text="+text;
+    return serverURL+"?"+"text="+text
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server,try again after some time");
 }
 function clickHandler(){
-    var inputText=txtInput.value;
+    var inputText=txtInput.value;//taking input
+
+    //calling server for processing
     fetch(getTranslation(inputText))
-    .then(response =>response.json)
-    .then(json => console.log(json))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText; //output
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click",clickHandler)
